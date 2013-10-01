b23a385223cfe9ac5699714cda62c2bc74f6aefc
gsereno <gsereno@KMAC02.local>
2013-10-01 14:31:46 -0700

    Base Code

---
+++ temperature1.py
@@ -0,0 +1,36 @@
+import os
+import glob
+import time
+os.system('modprobe w1-gpio')
+os.system('modprobe w1-therm')
+
+base_dir = '/sys/bus/w1/devices/'
+device_folder_0 = glob.glob(base_dir + '28*')[0]
+device_folder_1 = glob.glob(base_dir + '28*')[1]
+
+
+def read_temp_raw(* device_file):
+    f = open(device_file, 'r')
+    lines = f.readlines()
+    f.close()
+    return lines
+
+def read_temp(* device_file):
+    lines = read_temp_raw(device_file)
+    # print lines
+    while lines[0].strip()[-3:] != 'YES':
+        time.sleep(0.2)
+        lines = read_temp_raw()
+    equals_pos = lines[1].find('t=')
+    if equals_pos != -1:
+        temp_string = lines[1][equals_pos+2:]
+        temp_c = float(temp_string) / 1000.0
+        temp_f = temp_c * 9.0 / 5.0 + 32.0
+        return temp_c, temp_f
+
+while True:
+#    device_file = device_folder_0 + '/w1_slave'
+    print("Sensor 0 - ", device_folder_0, "  ", read_temp(device_folder_0 + '/w1_slave'))
+#    device_file = device_folder_1 + '/w1_slave'
+    print("Sensor 1 - ", device_folder_1, "  ", read_temp(device_folder_1 + '/w1_slave'))
+    time.sleep(1)
